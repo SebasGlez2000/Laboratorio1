e175d08ae2b320a069c8b025a916e0265d28dbb8
Sebastian <sebastiancroft01@gmail.com>
2022-09-08 22:23:23 -0500

    Laboratorio 1 completo

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # Aquí se manda a llamar todas las funciones y visualizaciones.
 
 import data as dt
+import visualizations as vs
 
 # Inversión pasiva
 dt.df_pasiva
+vs.Rend1()
+vs.Rend2()
 
 # Inversión activa
 dt.df_activa
+vs.Rend3()
+vs.Rend4()
 
 # Histórico de operaciones 
 dt.df_operaciones
